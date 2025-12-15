b9b8e3b209047c4c82524dbeb48624bea01b8c17
Tiago <tiagodesousacosta2024@gmail.com>
2025-12-14 21:13:03 -0300

    Enable auto superuser creation via AppConfig.ready

--- todos/apps.py
+++ todos/apps.py
@@ -1,5 +1,31 @@
 from django.apps import AppConfig
+import os
 
 
 class TodosConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "todos"
+
+    def ready(self):
+        if os.getenv("CREATE_SUPERUSER") == "True":
+            try:
+                from django.contrib.auth import get_user_model
+
+                User = get_user_model()
+
+                username = os.getenv("SUPERUSER_USERNAME")
+                email = os.getenv("SUPERUSER_EMAIL")
+                password = os.getenv("SUPERUSER_PASSWORD")
+
+                if username and password:
+                    if not User.objects.filter(username=username).exists():
+                        User.objects.create_superuser(
+                            username=username,
+                            email=email,
+                            password=password,
+                        )
+                        print("✅ Superusuário criado automaticamente")
+                    else:
+                        print("ℹ️ Superusuário já existe")
+            except Exception as e:
+                print("⚠️ Erro ao criar superusuário:", e)
